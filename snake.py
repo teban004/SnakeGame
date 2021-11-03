fc7c0d97a15eb03fe4aab32523ce189e044036d2
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-03 00:59:36 -0400

    Finished first playable version

--- snake.py
+++ snake.py
@@ -18,7 +18,7 @@ snake = [
     [snk_y, snk_x-2]
 ]
 
-food = [sh/2, sw/2]  #set the initial food place
+food = [round(sh/2), round(sw/2)]  #set the initial food place
 w.addch(food[0], food[1], curses.ACS_PI)  # add the food to the screen
 
 key = curses.KEY_RIGHT  # set initial direction of the snake
@@ -29,7 +29,7 @@ while True:
     key = key if next_key == -1 else next_key  # update the key value only when there is a new key
 
     # check if the user has lost the game
-    if snake[0][0] in [0, sh] or snake[0,1] in [0, sw] or snake[0] in snake[1:]:
+    if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
         curses.endwin()
         quit()
 
@@ -41,12 +41,26 @@ while True:
     if key == curses.KEY_UP:
         new_head[0] -= 1
     if key == curses.KEY_LEFT:
-        new_head[0] -= 1
+        new_head[1] -= 1
     if key == curses.KEY_RIGHT:
-        new_head[0] += 1
+        new_head[1] += 1
 
     # insert the new head of the snake
     snake.insert(0, new_head)
 
+    # check if the snake found food
+    if snake[0] == food:
+        food = None
+        while food is None:
+            nf = [
+                random.randint(1, sh-1),
+                random.randint(1, sw-1)
+            ]
+            food = nf if nf not in snake else None
+        w.addch(round(food[0]), round(food[1]), curses.ACS_PI)  # add the food
+    else:
+        tail = snake.pop()
+        w.addch(round(tail[0]), round(tail[1]), ' ')
     
+    w.addch(round(snake[0][0]), round(snake[0][1]), curses.ACS_CKBOARD)
 
